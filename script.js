3c105f570894016b69b1bf4ee752c29479ccf6f9
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-09-21 11:19:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.getElementById('loginBtn').addEventListener('click', function(){
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
+
+	if(!email || !password){
+	alert('Please enter both email/phone and password.');
+    return;
+	}
+
+	alert(`Logging in with: ${email}`);
+})
+
+// Handle Create Account button click
+document.getElementById('createAccountBtn').addEventListener('click', function () {
+  window.location.href = 'signup.html'; 
+});
